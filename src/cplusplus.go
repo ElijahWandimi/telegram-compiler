a86f8e5419535aa382d7b382516e09b5eb97a413
oyamo <oyamo.xyz@gmail.com>
2022-10-22 13:58:06 +0300

    create telegram creds

--- src/cplusplus.go
+++ src/cplusplus.go
@@ -8,12 +8,10 @@ import (
 
 
 func CPlus(code string) (*Response, error) {
-
 	payload, err := ConstructPayload(code, "cpp")
 	if err != nil {
 		return nil, err
 	}
-
 	headers := map[string]string{
 		"User-Agent":   "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
 	}
---
+++ src/telegram_update.go
@@ -0,0 +1,106 @@
+package src
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+)
+
+type Chat struct {
+	Id int `json:"id"`
+}
+
+type Message struct {
+	Text     string   `json:"text"`
+	Chat     Chat     `json:"chat"`
+}
+
+type Update struct {
+	UpdateId int     `json:"update_id"`
+	Message  Message `json:"message"`
+}
+
+func ParseTelegramRequest(r *http.Request) (*Update, error) {
+	var update Update
+	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
+		log.Printf("could not decode incoming update %s", err.Error())
+		return nil, err
+	}
+	return &update, nil
+}
+
+func sendTextToTelegramChat(chatId int, text string) (string, error) {
+
+	log.Printf("Sending %s to chat_id: %d", text, chatId)
+	var telegramApi string = "https://api.telegram.org/bot" + os.Getenv("TELEGRAM_BOT_TOKEN") + "/sendMessage"
+	response, err := http.PostForm(
+		telegramApi,
+		url.Values{
+			"chat_id": {strconv.Itoa(chatId)},
+			"text":    {text},
+		})
+
+	if err != nil {
+		log.Printf("error when posting text to the chat: %s", err.Error())
+		return "", err
+	}
+	defer response.Body.Close()
+
+	var bodyBytes, errRead = ioutil.ReadAll(response.Body)
+	if errRead != nil {
+		log.Printf("error in parsing telegram answer %s", errRead.Error())
+		return "", err
+	}
+	bodyString := string(bodyBytes)
+	log.Printf("Body of Telegram Response: %s", bodyString)
+
+	return bodyString, nil
+}
+
+func HandleTelegramWebHook(w http.ResponseWriter, r *http.Request) {
+
+	// Parse incoming request
+	var update, err = parseTelegramRequest(r)
+	if err != nil {
+		log.Printf("error parsing update, %s", err.Error())
+		return
+	}
+
+	// Sanitize input
+	var sanitizedSeed = sanitize(update.Message.Text)
+
+	// Call RapLyrics to get a punchline
+	var lyric, errRapLyrics = getPunchline(sanitizedSeed)
+	if errRapLyrics != nil {
+		log.Printf("got error when calling RapLyrics API %s", errRapLyrics.Error())
+		return
+	}
+
+	// Send the punchline back to Telegram
+	var telegramResponseBody, errTelegram = sendTextToTelegramChat(update.Message.Chat.Id, lyric)
+	if errTelegram != nil {
+		log.Printf("got error %s from telegram, reponse body is %s", errTelegram.Error(), telegramResponseBody)
+	} else {
+		log.Printf("punchline %s successfuly distributed to chat id %d", lyric, update.Message.Chat.Id)
+	}
+}	
+
+// parseTelegramRequest handles incoming update from the Telegram web hook
+func parseTelegramRequest(r *http.Request) (*Update, error) {
+	var update Update
+	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
+		log.Printf("could not decode incoming update %s", err.Error())
+		return nil, err
+	}
+	if update.UpdateId == 0 {
+		log.Printf("invalid update id, got update id = 0")
+		return nil, errors.New("invalid update id of 0 indicates failure to parse incoming update")
+	}
+	return &update, nil
+}
+
